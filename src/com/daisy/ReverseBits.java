4562566cffe3e8a76838d5d5eef55790796b53a7
7huang14 <7huang14@gmail.com>
2018-06-29 17:17:17 -0700

    sqer(x) 69

---
+++ src/com/daisy/ReverseBits.java
@@ -0,0 +1,17 @@
+package com.daisy;
+
+public class ReverseBits {
+    public int reverseBits(int n) {
+        if(n==0) return 0;
+        int res = 0;
+        for(int i=0; i<32; i++){
+
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println( 0 & 1);
+    }
+}
