e0ea4991c2d421a4326bdcb5fde2ed88cd206642
7huang14 <7huang14@gmail.com>
2018-07-01 22:33:24 -0700

    sqer(x) 69

---
+++ src/com/daisy/ValidAnagram.java
@@ -0,0 +1,22 @@
+package com.daisy;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    private static boolean isAnagram(String s, String t){
+        if(s.length() != t.length()) return false;
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for(int i=0; i<str1.length; i++){
+            if(str1[i] != str2[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isAnagram("acr", "cra"));
+    }
+
+}
